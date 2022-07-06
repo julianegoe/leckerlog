bc85dae9558d10be2e3059fb9829f283ab938cce
Juliane Görsch <goersch.juliane@dpa.com>
2022-07-06 22:05:36 +0200

    fixed bug

--- src/components/AppDateInput.vue
+++ src/components/AppDateInput.vue
@@ -5,18 +5,18 @@ import { ref } from 'vue';
 const props = defineProps<{
     label: string;
     id: string;
-    modelValue: string | null;
+    modelValue: string;
 }>()
 const emit = defineEmits(['update:modelValue']);
 
-const inputValue = ref<string | null>(props.modelValue);
-const updateInput = (value: string | null) => {
+const inputValue = ref(props.modelValue);
+const updateInput = (value: string) => {
     emit('update:modelValue',value);
 }
 </script>
 <template>
     <label class="relative -bottom-3 left-4 px-1 bg-white text-sm" :for="id">{{ label }}</label>
-    <input  @change="updateInput(inputValue)" v-model="inputValue" :id="id" :ame="id" class="py-2 px-4 border border-black w-full" type="date" placeholder="dd-mm-yyyy" />
+    <input  @change="updateInput(inputValue)" v-model="inputValue" :id="id" :name="id" class="py-2 px-4 border border-black w-full" type="date" placeholder="dd-mm-yyyy" />
 </template>
 <style scoped>
     input[type="date"]
