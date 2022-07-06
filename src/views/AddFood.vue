e6fb9126ee3fa4f13a4089662192edc851e2f46a
Juliane Görsch <goersch.juliane@dpa.com>
2022-07-06 23:48:19 +0200

    image preview before upload

--- src/views/AddFood.vue
+++ src/views/AddFood.vue
@@ -34,9 +34,11 @@ const inputValues = computed<LeckerLog>(() => (
 ));
 
 const arrayBuffer = vref();
+const imageFile = vref();
 const handlePhotoChange = (e: any) => {
   const fileList = e.target?.files;
   inputValues.value.restaurant.foodOrdered[0].fileName = fileList[0].name;
+  imageFile.value = URL.createObjectURL(fileList[0]);
   // Check if the file is an image.
   if (fileList[0].type && !fileList[0].type.startsWith('image/')) {
     console.log('File is not an image.', fileList[0].type, fileList[0]);
@@ -70,8 +72,13 @@ const addFood = () => {
             <AppDateInput v-model="inputValues.restaurant.foodOrdered[0].dateCreated" label="Bestellt am" id="date-input" />
             <AppNumberInput v-model="inputValues.restaurant.foodOrdered[0].rating" label="Bewertung" id="rating-input"/>
             <AppTextInput v-model="inputValues.restaurant.foodOrdered[0].comment" label="Kommentar" id="comment-input"/>
-            <button class="mt-4 p-2 border border-black" type="submit">Hinzufügen</button>
+            <div class="py-4">
+              <label for="file-upload" class="mt-4 p-2 border border-black active:bg-gray-200">Bild auswählen</label>
+              <img v-if="imageFile" class="mt-4" :src="imageFile" alt="selected-image" />
+              <input class="hidden" role="button" id="file-upload" type="file" @change="handlePhotoChange" accept="image/png, image/jpeg" />
+            </div>
+
+            <button class="mt-4 p-2 border border-black active:bg-gray-200" type="submit">Hinzufügen</button>
         </form>
-        <input type="file" @change="handlePhotoChange" accept="image/png, image/jpeg" />
     </div>
 </template>
